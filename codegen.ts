cda0535f641ab4790834fc1dba4f21ea0ef32479
jhangmez <jhangomez25@gmail.com>
2023-11-18 21:56:16 -0500

    Se modifico el graphql

--- codegen.ts
+++ codegen.ts
@@ -3,19 +3,13 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
   schema: 'http://localhost:3000/api/graphql',
   documents: ['src/**/*.ts'],
+  ignoreNoDocuments: true,
   generates: {
     './src/app/lib/gql/': {
       presetConfig: {
         gqlTagName: 'gql'
       },
-      preset: 'client',
-      plugins: [
-        'typescript', // genera tipos de TypeScript para tu esquema de GraphQL
-        'typescript-operations' // genera tipos de TypeScript para tus operaciones de GraphQL
-      ]
-    },
-    './graphql.schema.json': {
-      plugins: ['introspection']
+      preset: 'client'
     }
   }
 }
--- src/app/lib/gql/graphql.ts
+++ src/app/lib/gql/graphql.ts
@@ -1,64 +1,95 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>
+}
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>
+}
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T
+> = { [_ in K]?: never }
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never
+    }
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-};
+  ID: { input: string; output: string }
+  String: { input: string; output: string }
+  Boolean: { input: boolean; output: boolean }
+  Int: { input: number; output: number }
+  Float: { input: number; output: number }
+}
 
 export type Query = {
-  __typename?: 'Query';
-  todos: Array<User>;
-};
+  __typename?: 'Query'
+  todos: Array<User>
+  todosperfil: Array<Personalinformation>
+}
+
+export type Personalinformation = {
+  __typename?: 'personalinformation'
+  firstname: Scalars['String']['output']
+  gender: Scalars['String']['output']
+  id: Scalars['Int']['output']
+  lastname: Scalars['String']['output']
+  username: Scalars['String']['output']
+}
 
 export type User = {
-  __typename?: 'user';
-  email: Scalars['String']['output'];
-  id: Scalars['Int']['output'];
-  perinfId: Scalars['Int']['output'];
-  phonenumber: Scalars['String']['output'];
-};
+  __typename?: 'user'
+  email: Scalars['String']['output']
+  id: Scalars['Int']['output']
+  perinfId: Scalars['Int']['output']
+  phonenumber: Scalars['String']['output']
+}
 
-export type QueryQueryVariables = Exact<{ [key: string]: never; }>;
+export type QueryQueryVariables = Exact<{ [key: string]: never }>
 
+export type QueryQuery = {
+  __typename?: 'Query'
+  todos: Array<{
+    __typename?: 'user'
+    id: number
+    email: string
+    perinfId: number
+    phonenumber: string
+  }>
+}
 
-export type QueryQuery = { __typename?: 'Query', todos: Array<{ __typename?: 'user', id: number, email: string, perinfId: number, phonenumber: string }> };
-
-
-export const QueryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Query"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"todos"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"email"}},{"kind":"Field","name":{"kind":"Name","value":"perinfId"}},{"kind":"Field","name":{"kind":"Name","value":"phonenumber"}}]}}]}}]} as unknown as DocumentNode<QueryQuery, QueryQueryVariables>;
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-};
-
-export type Query = {
-  __typename?: 'Query';
-  todos: Array<User>;
-};
-
-export type User = {
-  __typename?: 'user';
-  email: Scalars['String']['output'];
-  id: Scalars['Int']['output'];
-  perinfId: Scalars['Int']['output'];
-  phonenumber: Scalars['String']['output'];
-};
-
-export type QueryQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type QueryQuery = { __typename?: 'Query', todos: Array<{ __typename?: 'user', id: number, email: string, perinfId: number, phonenumber: string }> };
+export const QueryDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'Query' },
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'todos' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'email' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'perinfId' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'phonenumber' } }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+} as unknown as DocumentNode<QueryQuery, QueryQueryVariables>
